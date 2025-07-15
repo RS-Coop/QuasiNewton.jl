c0001e38d5dbf3664bb432ad51db2c058d3c20d5
Cooper <simpson.r.cooper@gmail.com>
2025-07-14 18:53:21 -0700

    Trying reg linesearch.

--- src/linesearch.jl
+++ src/linesearch.jl
@@ -36,7 +36,7 @@ function backtrack!(opt::Optimizer, stats::Stats, x::S, f::F1, fg!::F2, fval::T,
 
     p .*= α
 
-    opt.M = α == 1. ? 2*opt.M : max(min(1e8, opt.M/α^2), 1e-8)
+    # opt.M = α == 1. ? 2*opt.M : max(min(1e8, opt.M/α^2), 1e-8) #Tried this, but it didn't work that well
 
     return success
 end
@@ -44,7 +44,7 @@ end
 ########################################################
 
 function search!(opt::SFNOptimizer, stats::Stats, x::S, f::F1, fg!::F2, fval::T, g::S, g_norm::T, Hv::H) where {F1<:Function, F2<:Function, T<:AbstractFloat, S<:AbstractVector{T}, H<:HvpOperator}
-    return backtrack!(opt, stats, x, f, fg!, fval, g, g_norm, Hv)
+    return search_M!(opt, stats, x, f, fg!, fval, g, g_norm, Hv)
 end
 
 ########################################################
