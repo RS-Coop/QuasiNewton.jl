16d31fa2d8ef899e2fea2838fce9b4356919a19b
Cooper <simpson.r.cooper@gmail.com>
2025-07-29 14:36:32 -0700

    Going to try other linesearch and restoring M estimation when given NaN.

--- src/minimize.jl
+++ src/minimize.jl
@@ -99,26 +99,27 @@ function iterate!(opt::O, x::S, f::F1, fg!::F2, Hv::H, itmax::I, time_limit::T) 
     g_norm = norm(grads)
 
     #Estimate regularization
-    #NOTE: Update this with Jensen type inequality
     if isnan(opt.M)
-        # ζ = randn(length(x))
-        # D = norm(ζ)^2
+        ζ = randn(length(x))
+        D = norm(ζ)^2
 
-        # g2 = similar(grads)
-        # fg!(g2, x+ζ)
+        g2 = similar(grads)
+        fg!(g2, x+ζ)
 
-        # if any(isnan.(g2))
-        #     opt.M = 1e15
-        # else
-        #     apply!(ζ, Hv, ζ) 
-        #     ζ .= g2-grads-ζ
+        if any(isnan.(g2))
+            opt.M = 1e-8
+        else
+            apply!(ζ, Hv, ζ) 
+            ζ .= g2-grads-ζ
 
-        #     opt.M = min(1e8, 2*norm(ζ)/(D))
-        # end
+            opt.M = min(1e8, 2*norm(ζ)/(D))
+        end
 
-        # g2 = nothing #mark for collection
+        g2 = nothing #mark for collection
 
-        opt.M = inv(g_norm)
+        # println("Estimated Hessian Lipschitz constant: ", opt.M)
+
+        # opt.M = inv(g_norm)
     end
 
     #Tolerance
