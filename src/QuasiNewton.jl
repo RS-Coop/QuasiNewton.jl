37774b2f5725c7fcb689b381c12098b52649c3d1
Cooper <simpson.r.cooper@gmail.com>
2025-07-31 16:52:15 -0700

    Fixing some isssues in the convenience function definitions.

--- src/QuasiNewton.jl
+++ src/QuasiNewton.jl
@@ -57,7 +57,7 @@ function optimize!(x::S, f::F1, fg!::F2, H::F3; itmax::I, time_limit::T=Inf, ato
 end
 
 #R-SFN
-function rsfn!(x::S, f::F; mode::Symbol, itmax::I, time_limit::T2=Inf, M::T1=1e-8, atol::T2=1e-5, rtol::T2=1e-6, linesearch::Bool=false, kwargs...) where {T1<:Real, T2<:AbstractFloat, S<:AbstractVector{T2}, F, I}
+function rsfn!(x::S, f::F; mode::Symbol=:LFASolver, itmax::I=1000, time_limit::T2=Inf, M::T1=1e-8, atol::T2=1e-5, rtol::T2=1e-6, linesearch::Bool=false, kwargs...) where {T1<:Real, T2<:AbstractFloat, S<:AbstractVector{T2}, F, I}
 	opt = SFNOptimizer(size(x,1), mode; M=M, linesearch=linesearch, atol=atol, rtol=rtol, kwargs...)
 
 	stats = minimize!(opt, x, f, itmax=itmax, time_limit=time_limit)
@@ -65,7 +65,7 @@ function rsfn!(x::S, f::F; mode::Symbol, itmax::I, time_limit::T2=Inf, M::T1=1e-
 	return stats
 end
 
-function rsfn!(x::S, f::F1, fg!::F2, H::L; mode::Symbol, itmax::I, time_limit::T2=Inf, M::T1=1e-8, atol::T2=1e-5, rtol::T2=1e-6, linesearch::Bool=false, kwargs...) where {T1<:Real, T2<:AbstractFloat, S<:AbstractVector{T2}, F1, F2, L, I}
+function rsfn!(x::S, f::F1, fg!::F2, H::L; mode::Symbol=:LFASolver, itmax::I=1000, time_limit::T2=Inf, M::T1=1e-8, atol::T2=1e-5, rtol::T2=1e-6, linesearch::Bool=false, kwargs...) where {T1<:Real, T2<:AbstractFloat, S<:AbstractVector{T2}, F1, F2, L, I}
 	opt = SFNOptimizer(size(x,1), mode; M=M, linesearch=linesearch, atol=atol, rtol=rtol, kwargs...)
 
 	stats = minimize!(opt, x, f, fg!, H, itmax=itmax, time_limit=time_limit)
@@ -74,7 +74,7 @@ function rsfn!(x::S, f::F1, fg!::F2, H::L; mode::Symbol, itmax::I, time_limit::T
 end
 
 #ARC
-function arc!(x::S, f::F; itmax::I, time_limit::T=Inf, atol::T=1e-5, rtol::T=1e-6) where {T<:AbstractFloat, S<:AbstractVector{T}, F, I}
+function arc!(x::S, f::F; itmax::I=1000, time_limit::T=Inf, atol::T=1e-5, rtol::T=1e-6) where {T<:AbstractFloat, S<:AbstractVector{T}, F, I}
 	opt = ARCOptimizer(size(x,1), atol=atol, rtol=rtol)
 
 	stats = minimize!(opt, x, f; itmax=itmax, time_limit=time_limit)
@@ -82,7 +82,7 @@ function arc!(x::S, f::F; itmax::I, time_limit::T=Inf, atol::T=1e-5, rtol::T=1e-
 	return stats
 end
 
-function arc!(x::S, f::F1, fg!::F2, H::L; itmax::I, time_limit::T=Inf, atol::T=1e-5, rtol::T=1e-6) where {T<:AbstractFloat, S<:AbstractVector{T}, F1, F2, L, I}
+function arc!(x::S, f::F1, fg!::F2, H::L; itmax::I=1000, time_limit::T=Inf, atol::T=1e-5, rtol::T=1e-6) where {T<:AbstractFloat, S<:AbstractVector{T}, F1, F2, L, I}
 	opt = ARCOptimizer(size(x,1), atol=atol, rtol=rtol)
 
 	stats = minimize!(opt, x, f, fg!, H; itmax=itmax, time_limit=time_limit)
@@ -91,7 +91,7 @@ function arc!(x::S, f::F1, fg!::F2, H::L; itmax::I, time_limit::T=Inf, atol::T=1
 end
 
 #Newton
-function newton!(x::S, f::F; posdef::Bool=false, linesearch::Bool=false, itmax::I, time_limit::T=Inf, atol::T=1e-5, rtol::T=1e-6) where {T<:AbstractFloat, S<:AbstractVector{T}, F, I}
+function newton!(x::S, f::F; itmax::I=1000, time_limit::T=Inf, posdef::Bool=false, linesearch::Bool=false, atol::T=1e-5, rtol::T=1e-6) where {T<:AbstractFloat, S<:AbstractVector{T}, F, I}
 	opt = NewtonOptimizer(size(x,1), posdef=posdef, linesearch=linesearch, atol=atol, rtol=rtol)
 
 	stats = minimize!(opt, x, f; itmax=itmax, time_limit=time_limit)
@@ -99,7 +99,7 @@ function newton!(x::S, f::F; posdef::Bool=false, linesearch::Bool=false, itmax::
 	return stats
 end
 
-function newton!(x::S, f::F1, fg!::F2, H::L; posdef::Bool=false, linesearch::Bool=false, itmax::I, time_limit::T=Inf, atol::T=1e-5, rtol::T=1e-6) where {T<:AbstractFloat, S<:AbstractVector{T}, F1, F2, L, I}
+function newton!(x::S, f::F1, fg!::F2, H::L; itmax::I=1000, time_limit::T=Inf, posdef::Bool=false, linesearch::Bool=false, atol::T=1e-5, rtol::T=1e-6) where {T<:AbstractFloat, S<:AbstractVector{T}, F1, F2, L, I}
 	opt = NewtonOptimizer(size(x,1), posdef=posdef, linesearch=linesearch, atol=atol, rtol=rtol)
 
 	stats = minimize!(opt, x, f, fg!, H; itmax=itmax, time_limit=time_limit)
