22bda99139ef502438cf1fdaf8b12144018cba27
Cooper <simpson.r.cooper@gmail.com>
2026-08-03 11:27:52 -0700

    Adding fallback.

--- src/optimizers/rsfn.jl
+++ src/optimizers/rsfn.jl
@@ -489,6 +489,8 @@ function search_M!(opt::RSFNOptimizer, x::S, p!::F, obj::Objective, stats::Quasi
     status = false
     f0 = obj.fval
 
+    M_prev = opt.M
+
     # 
     for M in 10.0 .^ (-8:8)
         opt.M = M
@@ -500,7 +502,15 @@ function search_M!(opt::RSFNOptimizer, x::S, p!::F, obj::Objective, stats::Quasi
         end
     end
 
-    return status
+    if status
+        return status
+    else
+        opt.M = M_prev
+        p, _ = p!()
+        η, status = armijo!(opt, x, p, obj, stats)
+        p .*= η
+        return status
+    end
 end
 
 """
